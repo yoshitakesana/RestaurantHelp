db5a76775b60efed5d8084d548fe68dca05fe015
K-Takahashiiii <fko2447008@stu.o-hara.ac.jp>
2025-09-12 14:04:35 +0900

    初期設定

--- RestaurantHelp/settings.py
+++ RestaurantHelp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restauranthelp.apps.RestaurantHelpAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'utf-8'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'ja'
 
 USE_I18N = True
 

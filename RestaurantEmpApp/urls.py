f141b0af2f67f5ed9747e76faeb634fcfbb23acc
yoshitakesana <fko2447057@stu.o-hara.ac.jp>
2025-09-17 14:11:38 +0900

    ３つのアプリの必要なhtmlファイルを作成しました

--- RestaurantEmpApp/urls.py
+++ RestaurantEmpApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView#,ListView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    #path('EditEmployee/',ListView.as_view(),name='EditEmployee'),
 ]

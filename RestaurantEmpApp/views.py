c27d401c6ade3d425d1318002acdff92e523d6b9
yoshitakesana <fko2447057@stu.o-hara.ac.jp>
2025-09-17 15:10:57 +0900

    add

--- RestaurantEmpApp/views.py
+++ RestaurantEmpApp/views.py
@@ -1,4 +1,9 @@
 from django.views.generic import TemplateView
 
+# トップページ
 class IndexView(TemplateView):
     template_name = 'RestaurantEmpApp/index.html'
+
+# 従業員編集ページ（仮）
+class EditEmployeeView(TemplateView):
+    template_name = 'RestaurantEmpApp/Edit Employee.html'

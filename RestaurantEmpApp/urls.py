4498ebb0884a0e16bb84697f047bb19384e2dfaf
yoshitakesana <fko2447057@stu.o-hara.ac.jp>
2025-09-19 11:17:24 +0900

    RestaurantEmpの仮のurlとviews書きました

--- RestaurantEmpApp/urls.py
+++ RestaurantEmpApp/urls.py
@@ -1,7 +1,27 @@
 from django.urls import path
-from .views import IndexView, EditEmployeeView  # EditEmployeeView を忘れずにインポート
+from .views import IndexView, EditEmployeeView, MenuView,AddEmployeeView,DetailEmployeeView,EditMenuView,DetailMenuView,AddMenuView,EditManagerView
 
 urlpatterns = [
+      # トップページ
     path('', IndexView.as_view(), name='index'),
-    path('EditEmployee/', EditEmployeeView.as_view(), name='EditEmployee'),
+     # メニュー画面
+    path('menu/', MenuView.as_view(), name='menu'), 
+     # 従業員編集画面
+    path('EditEmployee/', EditEmployeeView.as_view(), name='EditEmployee'), 
+    #従業員追加画面
+    path('AddEmployee/',AddEmployeeView.as_view(),name='AddEmployee'),
+    #選択した従業員の詳細画面
+    path('DetailEmployee/',DetailEmployeeView.as_view(),name='DetailEmployee'),
+
+    #メニュー編集ページ
+    path('EditMenu/',EditMenuView.as_view(),name='EditMenu'),
+    #メニュー詳細ページ
+    path('DetailMenu/',DetailMenuView.as_view(),name='DetailMenu'),
+    #メニュー追加ページ
+    path('AddMenu/',AddMenuView.as_view(),name='AddMenu'),
+    
+    #店長編集ページ
+    path('EditManager/',EditManagerView.as_view(),name='EditManager')
+
+
 ]
--- RestaurantEmpApp/views.py
+++ RestaurantEmpApp/views.py
@@ -1,9 +1,48 @@
 from django.views.generic import TemplateView
+from django.urls import reverse_lazy
 
-# トップページ
+
+# ログインページ
 class IndexView(TemplateView):
     template_name = 'RestaurantEmpApp/index.html'
 
-# 従業員編集ページ（仮）
+#社員さんのメニューページ
+class MenuView(TemplateView):
+    template_name='RestaurantEmpApp/menu.html'
+
+# 従業員編集ページ（本当はUpdateVIew）
 class EditEmployeeView(TemplateView):
-    template_name = 'RestaurantEmpApp/Edit Employee.html'
+    template_name = 'RestaurantEmpApp/EditEmployee.html'
+    ##success_url = reverse_lazy('menu')
+
+#従業員追加ページ
+class AddEmployeeView(TemplateView):
+    template_name='RestaurantEmpApp/AddEmployee.html'
+
+#従業員詳細ページ
+class DetailEmployeeView(TemplateView):
+    template_name='RestaurantEmpApp/DetailEmployee.html'
+
+
+
+#メニュー編集ページ
+class EditMenuView(TemplateView):
+    template_name='RestaurantEmpApp/EditMenu.html'
+
+#メニュー詳細ページ
+class DetailMenuView(TemplateView):
+    template_name='RestaurantEmpApp/DetailMenu.html'
+
+#メニュー追加ページ
+class AddMenuView(TemplateView):
+    template_name='RestaurantEmpApp/AddMenu.html'
+
+#社員編集ページ
+class EditManagerView(TemplateView):
+    template_name='RestaurantEmpApp/EditManager.html'
+
+
+
+
+
+
